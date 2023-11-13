585a39d6ebb7dbcd704168a41179eb6f34c81c69
ayrawn00 <ayrawn@gmail.com>
2023-11-13 16:59:12 -0600

    It's Day 1

---
+++ Name Generator.py
@@ -0,0 +1,5 @@
+# Band Name Generator
+print("Welcome To My Band Name Generator")
+name = input("What city did you grow up in?\n")
+pet = input("What is the name of your first pet?\n")
+print("Your band name could be " + name + " " + pet + "!")
